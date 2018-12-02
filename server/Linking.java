66d73879ef263a9543da86761c22f236f13a4b45
心叶 <yelloxing@gmail.com>
2018-12-02 17:56:47 +0800

    废除java服务器

--- server/Linking.java
+++
@@ -1,8 +0,0 @@
-public class Linking {
-    public static void main(String[] args) {
-
-        // 建立数据库操作对象
-        Sqler sqler = new Sqler("jdbc:mysql://127.0.0.1:3306/clay", "root", "254896");
-
-    }
-}
